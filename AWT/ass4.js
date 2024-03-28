b32f2ae49b1e740f1121ee4df76ea3f2c92715d0
Mangesh Ghadwaje <88280057+mangeshh09@users.noreply.github.com>
2024-03-27 22:51:53 -0700

    Add files via upload

---
+++ AWT/ass4.js
@@ -0,0 +1,9 @@
+//Design a simple API using Express.js
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("welcome to kkwagh");
+});
+app.listen(4000,()=>{
+    console.log("listening to port 4000");
+});
